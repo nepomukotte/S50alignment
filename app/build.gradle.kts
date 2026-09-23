57c72769f04c5004608172533b30eb0f722c6771
nepomukotte <nepomuk.otte@gmail.com>
2026-09-23 15:48:08 -0400

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "edu.gatech.s50polaralign"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "edu.gatech.s50polaralign"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
